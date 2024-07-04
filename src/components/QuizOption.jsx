52c0af6cc01159d468b9268330c610f39b22a3fc
emanuel-phillipe <emanuelphillipe1706@gmail.com>
2024-07-04 09:00:05 -0300

    Responsive design

--- src/components/QuizOption.jsx
+++ src/components/QuizOption.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function QuizOption({title, desc, click, disabled}) {
 
   const classStyles = {
-    div: disabled ? "bg-zinc-50 rounded-[0.5rem] p-4 w-max transition-all cursor-not-allowed" : "bg-zinc-100 rounded-[0.5rem] p-3 px-4 w-max hover:bg-zinc-200 transition-all cursor-pointer",
+    div: disabled ? "bg-zinc-50 rounded-[0.5rem] p-4 w-full md:w-max transition-all cursor-not-allowed" : "bg-zinc-100 rounded-[0.5rem] p-3 px-4 w-full md:w-max hover:bg-zinc-200 transition-all cursor-pointer",
     title: disabled ? "text-[1.1rem] font-semibold text-zinc-500" : "text-[1.1rem] font-semibold",
     desc: disabled ? "text-[0.9rem] text-zinc-400" : "text-[0.9rem] text-zinc-600"
   }
