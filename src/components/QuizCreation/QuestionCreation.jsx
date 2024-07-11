f704af325571188361172c0a774c754c978af050
Edugas-ai <emanuelphillipe@ninaservices.cloud>
2024-07-10 21:37:30 -0300

    Added protections to CreateButtons

--- src/components/QuizCreation/QuestionCreation.jsx
+++ src/components/QuizCreation/QuestionCreation.jsx
@@ -60,11 +60,13 @@ export function QuestionCreation() {
     })
   }
 
-  const ableToSave = quizValues.title != "" && quizValues.nameCreator != "" && quizValues.questions.length > 0
+  const ableToSave = quizValues.title && quizValues.nameCreator && quizValues.questions.length > 0
   const buttonStyle = ableToSave ? "p-2 px-4 rounded-lg cursor-pointer font-medium bg-zinc-100 hover:bg-zinc-200 transition-all" : "p-2 px-4 text-zinc-600 rounded-lg cursor-not-allowed font-medium bg-zinc-100 transition-all"
   const buttonStyleMobile = ableToSave ? "p-4 w-full rounded-lg cursor-pointer font-medium bg-zinc-100  transition-all" : "p-4 w-full rounded-lg cursor-not-allowed font-medium bg-zinc-100  transition-all"
 
   const createQuiz = async () => {
+
+    if(!ableToSave) return false
 
     const response = await fetch(import.meta.env.VITE_API + "/create", {
       method: "POST",
